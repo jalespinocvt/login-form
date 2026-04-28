85270c0fecdd846bf809370f9864dc3b73a010a1
jalespinocvt <jalespino.cvt@eac.edu.ph>
2026-04-28 13:22:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 function login() {
   const user = document.getElementById("username").value;
   const pass = document.getElementById("password").value;
+  console.log("Username:", user);
+  console.log("Password:", pass);
   if (user && pass) {
     alert("Login successful for user: " + user);
   } else {
